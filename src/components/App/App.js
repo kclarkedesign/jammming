7f6e25ad9ae4b6a7e0928b8ebd8f0dbb940355b3
Kirk Clarke <kclarke.design@gmail.com>
2018-05-27 12:16:59 -0400

    Added basic components and functionality for displaying tracks in search results and playlist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: "name",
+          artist: "artist",
+          album: "album",
+          id: 1 
+        },
+        {
+          name: "name",
+          artist: "artist",
+          album: "album",
+          id: 2 
+        },
+        {
+          name: "name",
+          artist: "artist",
+          album: "album",
+          id: 3 
+        }
+      ],
+      playlistTracks: [],
+      playlistName : ""
+    };
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+  }
+
+
+  addTrack(track) {
+    if(this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } else {
+      let newTrack = track.id;
+      this.state.playlistTracks.push(newTrack);
+      console.log(this.state.playlistTracks);
+    }
+  }
+
+  removeTrack(track) {
+    if(this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      this.state.playlistTracks = this.state.playlistTracks.filter(savedTrack => savedTrack.id !== track.id);
+      console.log(this.state.playlistTracks);
+    } else {
+      return;
+    }
+  }
+
+  updatePlaylistName(name) {
+    this.state.playlistName = name;
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Playlist.css';
+import Tracklist from '../Tracklist/Tracklist';
+
+class Playlist extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.handleNameChange = this.handleNameChange.bind(this);	
+	}
+
+	handleNameChange(event) {
+		this.props.onNameChange(event.target.value);
+	}
+
+	render() {
+		return (
+			<div className="Playlist">
+				<input defaultValue="New Playlist" onChange={this.handleNameChange}/>
+				<Tracklist tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true}/>
+				<a className="Playlist-save">SAVE TO SPOTIFY</a>
+			</div>
+		);
+	}
+}
+
+export default Playlist;
